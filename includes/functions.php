b19bdb9888eaaa006e5ba87616f159cacc632c86
Chris Haas <chris@vendiadvertising.com>
2020-12-17 18:45:24 -0600

    Fixed global function include

--- includes/functions.php
+++ includes/functions.php
@@ -2,7 +2,9 @@
 
 use Vendi\ImageUtilities\FlyPictureTagGenerator\PictureTagUtility;
 
-function vendi_fly_get_attachment_picture($attachment_id = 0, $size = '', $crop = null, $attr = []): string
-{
-    return PictureTagUtility::get_instance()->get_attachment_picture($attachment_id, $size, $crop, $attr);
+if (!function_exists('vendi_fly_get_attachment_picture')) {
+    function vendi_fly_get_attachment_picture($attachment_id = 0, $size = '', $crop = null, $attr = []): string
+    {
+        return PictureTagUtility::get_instance()->get_attachment_picture($attachment_id, $size, $crop, $attr);
+    }
 }
